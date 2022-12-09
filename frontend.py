5f6a640fd0a35ffb69e83688bc70633474bc1e19
Tebitz <thilo.butt@gmail.com>
2022-12-09 13:14:15 +0100

    möchte gern responsiv

--- frontend.py
+++ frontend.py
@@ -4,57 +4,70 @@ from random import randint
 #Karten
 farbe = ["yellow", "blue", "red", "green"]
 zahl = [0, 1, 2, 3, 4, 5, 6, 7]
-stapel = []
-stapel()
-stapel = [["green", "1"], ["blue", "2"], ["red", "3"], ["yellow", "4"]] #automatisch erstellen lassen
-hand = [["green", "1"], ["blue", "2"], ["red", "3"], ["yellow", "4"]]
+hand = []
+hand2 = []
 
 #stapel automatisch erstellen
-def stapel():
-    global stapel
+def stapelerstellen():
+    global stapel, stapelanzahl
+    stapel = []
+    stapelanzahl = 0
     for i in range(len(farbe)):
         for y in range(len(zahl)):
-            stapel = stapel + [farbe[i], zahl[y]]
-    print(stapel)
+            stapel = stapel + [[farbe[i], zahl[y]]]
+            stapelanzahl += 1
+    #print(stapel)
+    #print(stapelanzahl)
+stapelerstellen()
 
 #Variablen
-width = 1070
-y0 = 450
-y1 = 700
+width = 1900
+height = 980
+y0 = height-150
+y1 = y0+250
 
 # Ereignisbehandlung
 def aufnehmen(event):
-    random1 = randint(0,3)
-    random2 = randint(0,6)
+    global stapelanzahl, hand
+    random = randint(0, stapelanzahl)
+    #print(random)
+    hand = hand + [stapel[random]]
+    stapel.pop(random)
+    stapelanzahl -= 1
+    #print(stapel)
+    #print(stapelanzahl)
+    #print(hand)
+    ordnen()
 
-def ordnen(event):
+def ordnen():
+    #alte Karten müssen vorher gelöscht werden
     global x0, y0, x1, y1
     index = width / (len(hand)) 
     x0 = (index/2)-75
     x1 = x0 + 150
     for i in range(len(hand)):
-        print(index)
+        #print(index)
         rectangle = f"id_Karte{i}"
         canvas.create_rectangle(x0, y0, x1, y1, fill=hand[i][0]) #hand[i][1]]
         x0 += index
         x1 = x0 + 150
 
 def legen1(event):
-    canvas.coords("id_Karte1", (550, 100, 700, 350)) #funktioniert noch nicht
+    canvas.coords("id_Karte1", (550, 100, 700, 350)) #funktioniert  nicht
 
 # Erzeugung des Fensters
 tkFenster = Tk()
 tkFenster.title("Leinwand")
-tkFenster.geometry("1080x550")
+tkFenster.geometry(f"{width}x{height}")
 
 # Zeichenleinwand
 canvas = Canvas(master=tkFenster, background="brown")
-canvas.place(x=5, y=5, width=width, height=540)
+canvas.place(width=width, height=height)
 # Grafikobjekte
 id_karte = canvas.create_rectangle(550, 100, 700, 350, fill="black") #x0, y0, x1, y1
 id_stapel = canvas.create_rectangle(350, 100, 500, 350, fill="black") #x0, y0, x1, y1
 # Schaltfaechen
-tkFenster.bind('<KeyPress- >', ordnen)
+tkFenster.bind('<KeyPress- >', aufnehmen)
 tkFenster.bind('<KeyPress-1>', legen1)
 
 # Aktivierung der Ereignisschleife
