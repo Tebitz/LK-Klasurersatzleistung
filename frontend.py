0aed368d19b9b7678e8662c63b727185a7f9052a
Tebitz <thilo.butt@gmail.com>
2022-12-17 13:57:31 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -7,6 +7,10 @@ zahl = [0, 1, 2, 3, 4, 5, 6, 7]
 hand = []
 hand2 = []
 
+#Variablen
+y0=0
+y1=y0+250
+
 #stapel automatisch erstellen
 def stapelerstellen():
     global stapel, stapelanzahl
@@ -33,8 +37,13 @@ def aufnehmen(event):
     #print(hand)
     ordnen()
 
-def ordnen():
-    #canvascards.destroy()
+def neucanvascards():
+    canvascards.destroy()
+    canvascards = Canvas(master=tkFenster, background="black")
+    canvascards.place(y=height-250, width=width, height=250)
+
+def  ordnen():
+    #neucanvascards()
     global x0, y0, x1, y1
     index = width / (len(hand)) 
     x0 = (index/2)-75
@@ -56,19 +65,14 @@ tkFenster.attributes("-fullscreen", True)
 
 width = tkFenster.winfo_screenwidth()
 height = tkFenster.winfo_screenheight()
-#tkFenster.update()
-print(width)
-print(height)
-
-#Variablen
-y0 = height-150
-y1 = y0+250
+#print(width)
+#print(height)
 
 # Zeichenleinwand
 canvas = Canvas(master=tkFenster, background="gray")
-canvas.place(width=width, height=height-150)
+canvas.place(width=width, height=height-250)
 canvascards = Canvas(master=tkFenster, background="black")
-canvas.place(y=height-150, width=width, height=150)
+canvascards.place(y=height-250, width=width, height=250)
 # Grafikobjekte
 id_karte = canvas.create_rectangle(550, 100, 700, 350, fill="black") #x0, y0, x1, y1
 id_stapel = canvas.create_rectangle(350, 100, 500, 350, fill="black") #x0, y0, x1, y1
