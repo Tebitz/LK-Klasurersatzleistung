8dcdb7d136009394865e5acec957fbf1635dbc3e
Gottesanpeter <113508197+Gottesanpeter@users.noreply.github.com>
2023-01-05 17:15:27 +0100

    Add files via upload

---
+++ programm/functions.py
@@ -0,0 +1,25 @@
+def textdatei(parameter, value): #parameter = was gesucht wird (number_of_cards), value = in was der Wert geändert werden soll, beim auslesen value = "null"
+    text = open("settings.txt", "r")
+    data = text.read()
+    text.close()
+    data = data.split()
+    i = 0
+    while i < len(data)-1:
+        if data[i] == parameter:
+            if value == "null":
+                return(data[i + 1])
+            else:
+                i = i + 1
+                break
+        i = i + 2
+    data[i] = str(value)
+    text = open("settings.txt", "w")
+    for i in range(len(data)):
+        text.write(data[i] + "\n")
+    text.close()
+
+#print(textdatei("font_color_1", "null"))
+    if i >= len(data):
+        print(i)
+        print(data[i])
+        print(data)
